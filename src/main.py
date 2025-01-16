6299177b4ebdb209ecc1ec3d6b39864c34df6724
striderjg <JohnG27@yahoo.com>
2025-01-15 21:01:41 -0800

    Added a try copy from static to public

--- src/main.py
+++ src/main.py
@@ -1,6 +1,41 @@
 #!/usr/bin/env python3
 from textnode import *
 from leafnode import LeafNode
+from shutil import rmtree, copy
+import os
+
+GENERATED_DIR = "public"
+STATIC_DIR = "static"
+
+def tree_copy(src, dest):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    os.chdir(dir_path)
+    os.chdir("..")
+
+    if not os.path.exists(src):
+        raise ValueError("tree_copy(src, dest) was passed an invalid path")   
+    
+    if( os.path.exists(dest)):
+        rmtree(dest)
+    os.mkdir(dest)
+
+    __r_copy(src, dest)   
+    return
+
+def __r_copy(src, dest):
+    src_dir_path = os.path.join(os.getcwd(), src)
+    dir_contents = os.listdir(src)
+    for content in dir_contents:
+        content_path = os.path.join(src_dir_path, content)
+        if os.path.isfile(content_path):
+            copy(content_path, dest)
+        elif os.path.isdir(content_path):
+            new_dir = os.path.join(dest, content)
+            os.mkdir(new_dir)
+            __r_copy(content_path, new_dir)
+            
+            
+
 
 def text_node_to_html_node(text_node):
     match(text_node.text_type):
@@ -32,8 +67,7 @@ def image_to_html_node(node):
     return
 
 def main():
-    dummy = TextNode("This is a dumb text node", TextType.CODE)
-    print(dummy)
+    tree_copy(STATIC_DIR, GENERATED_DIR)
 
 
 
